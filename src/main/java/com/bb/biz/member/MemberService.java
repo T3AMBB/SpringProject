dd32d5ef57ea3e3c68326baeeb93d5087baf60f3
user-PC\Home <dlaaod012@naver.com>
2022-09-30 13:54:29 +0900

    Member DAO updateMileage 추가

--- src/main/java/com/bb/biz/member/MemberService.java
+++ src/main/java/com/bb/biz/member/MemberService.java
@@ -2,6 +2,7 @@ package com.bb.biz.member;
 
 
 public interface MemberService {
+	boolean updateMileage(MemberVO vo);
 	boolean insertMember(MemberVO vo);
 	MemberVO selectOneMember(MemberVO vo);
 	boolean updateMember(MemberVO vo);
--- src/main/java/com/bb/biz/member/impl/MemberServiceImpl.java
+++ src/main/java/com/bb/biz/member/impl/MemberServiceImpl.java
@@ -34,4 +34,10 @@ public class MemberServiceImpl implements MemberService{
 		
 	}
 
+	@Override
+	public boolean updateMileage(MemberVO vo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
